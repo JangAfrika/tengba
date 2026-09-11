e48c2bfa0727d36694e6a568306c48768e39e2e0
JangAfrika <info.jangafrica@gmail.com>
2026-09-11 15:05:12 +0000

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 // You do not need to change those two lines below.
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwQ-n7fWrSOV-I0xkZ0E6Xb7Jz54xAFI-wE3MXilN7wvRLWKoyy82iEZXR-mPdiVDUl0A/exec', // e.g. https://script.google.com/macros/s/AKfycb.../exec
+  API_URL: 'https://script.google.com/macros/s/AKfycbylSKOIqvH-VmKVTO5ODW9d9YQ_8AK9NfXwaUaI4eSzQMAhvcBelKY5yptuf66DahX3fQ/exec', // e.g. https://script.google.com/macros/s/AKfycb.../exec
 
   SPREADSHEET_ID: '1G9dUzuZTsdxsitkhu640Q-I0MQjN1gK3uNaZW2jYjms',
   RECEIPTS_FOLDER_ID: '1tXik6K7ONIFoy4LwXqa4phn4V2nCEHqX'
