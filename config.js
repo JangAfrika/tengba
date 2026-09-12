44a2de2c16e1109fd9e2d57654ebf261ac55a5e4
JangAfrika <info.jangafrica@gmail.com>
2026-09-12 23:21:39 +0000

    Change API_URL to new Google Apps Script endpoint
    Updated API_URL to point to a new endpoint.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 // You do not need to change those two lines below.
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyBODpEp_P_jnAxIaacxkrSQk2edgqk3LUUVT83MDxFFoH_gKRxh-Fln4CnG1QtQF_LNA/exec', // e.g. https://script.google.com/macros/s/AKfycb.../exec
+  API_URL: 'https://script.google.com/macros/s/AKfycbxLlP6ZNtmTT_-qnBgkZ1psFSBIQb3E3TPdhSSY4YEgpZssZNNtpgVMMmF4WxyMrhMQAg/exec', // e.g. https://script.google.com/macros/s/AKfycb.../exec
 
   SPREADSHEET_ID: '1G9dUzuZTsdxsitkhu640Q-I0MQjN1gK3uNaZW2jYjms',
   RECEIPTS_FOLDER_ID: '1tXik6K7ONIFoy4LwXqa4phn4V2nCEHqX'
